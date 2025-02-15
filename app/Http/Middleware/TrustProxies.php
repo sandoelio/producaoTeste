ec2698cef32f3ab4e77587947a175f4444f12231
Sandoelio Silva <sandoelio@hotmail.com>
2025-02-14 21:37:02 -0300

    Update TrustProxies.php

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -1,2 +1,16 @@
-protected $proxies = '*';
-protected $headers = Request::HEADER_X_FORWARDED_PROTO;
+namespace App\Http\Middleware;
+
+use Illuminate\Http\Middleware\TrustProxies as Middleware;
+use Illuminate\Http\Request;
+
+class TrustProxies extends Middleware
+{
+    protected $proxies = '*'; // Permite todos os proxies
+
+    protected $headers = Request::HEADER_X_FORWARDED_FOR
+                        | Request::HEADER_X_FORWARDED_HOST
+                        | Request::HEADER_X_FORWARDED_PORT
+                        | Request::HEADER_X_FORWARDED_PROTO
+                        | Request::HEADER_X_FORWARDED_AWS_ELB;
+}
+
